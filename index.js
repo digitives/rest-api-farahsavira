eab113c169d15ef6e5d9ec978e4290d49c6266fc
andikaleonardo <andika.leonardo@reksamadani.com>
2023-09-18 15:05:42 +0700

    fix index

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// var express = require('express'),
-//     app = express(),
-//     port = process.env.PORT || 8080,
-//     bodyParser = require('body-parser'),
-//     cors = require('cors');
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8080,
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mysql = require('mysql'); // Import the MySQL module
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 8080;
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const mysql = require('mysql'); // Import the MySQL module
 
 const httpProxy = require('http-proxy')
 // var proxy = httpProxy.createProxyServer(options);
