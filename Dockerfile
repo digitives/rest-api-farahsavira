bf03f548bba52d9849bbff3edaf6f28f5ad9f00c
andikaleonardo <andika.leonardo@reksamadani.com>
2023-09-18 13:04:11 +0700

    added docker-compose

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the official Node.js image as the base image
+FROM node:14
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the container
+COPY package*.json ./
+
+# Install Node.js dependencies
+RUN npm install
+
+# Copy the rest of the application code to the container
+COPY . .
+
+# Expose the port your application will run on
+EXPOSE 8080
+
+# Start the Node.js application
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -16,3 +16,32 @@ routes(app);
 
 app.listen(port);
 console.log('Thanks Kiddy!, RESTful API server started on: ' + port);
+
+
+// // index.js
+
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 8080;
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const mysql = require('mysql'); // Import the MySQL module
+
+// const db = mysql.createConnection({
+//   host: 'db', // Use the service name as the host
+//   user: 'your_database_user',
+//   password: 'your_database_password',
+//   database: 'your_database_name'
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log('Connected to the database');
+// });
+
+// // ... (rest of your code)
+
+// app.listen(port);
+// console.log('Thanks Kiddy!, RESTful API server started on: ' + port);
